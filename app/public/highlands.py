30f17703b9efe73aea627fbfe512f6f5ddcc0db4
h0lysp4nk <liamjosephkeenan@gmail.com>
2019-04-20 08:57:26 +0100

    Added Database Class, User Class. Code cleanup

--- app/public/highlands.py
+++ app/public/highlands.py
@@ -7,6 +7,56 @@ import sqlite3
 app = Flask(__name__)
 app.secret_key = os.urandom(2)
 
+""" Class for handling database connections, checks etc... """
+class Database:
+    dbpath = "/app/highlands.db"
+    def connect(self):
+        return sqlite3.connect(self.dbpath)
+
+    def setup(self):
+	user_table_sql = 'CREATE TABLE `users`( `id` INTEGER PRIMARY KEY AUTOINCREMENT, `username` VARCHAR(75) NOT NULL, `password` VARCHAR(75) NOT NULL, `firstname` VARCHAR(75) NOT NULL, `lastname` VARCHAR(75) NOT NULL, `email` VARCHAR(75) NOT NULL)'
+        default_user_sql = 'INSERT INTO users(username, password, firstname, lastname, email) VALUES("admin", "password", "Admin", "User", "admin@invalid.com")'
+
+	conn = self.connect()
+	c = conn.cursor()
+
+	c.execute(user_table_sql)
+	c.execute(default_user_sql)
+
+	conn.commit()
+	conn.close()
+
+    def __init__(self):
+        if os.path.exists(self.dbpath) == False:
+	    print "[?] Error, database wasn't found..."
+	    self.setup()
+	    print "[!] Done, database was created"
+""" Class for handling database interactions related to Users """
+class User:
+    def __init__(self):
+        pass
+
+    def login(self, POST_USERNAME, POST_PASSWORD):
+        conn = Database().connect()
+
+        c = conn.cursor()
+        c.execute("SELECT username,password FROM users WHERE username='{un}' AND password='{pw}'".format(un=POST_USERNAME, pw=POST_PASSWORD))
+
+        rows = c.fetchall()
+
+        usernameCorrect = False
+        passwordCorrect = False
+        for row in rows:
+            for value in row:
+                if POST_USERNAME == value:
+                    usernameCorrect = True
+                elif POST_PASSWORD == value:
+                    passwordCorrect = True
+
+        if usernameCorrect == True and passwordCorrect == True:
+            return True
+ 
+
 """ Default route """
 @app.route('/login')
 def login():
@@ -29,25 +79,7 @@ def logincheck():
     POST_USERNAME = str(request.form['username'])
     POST_PASSWORD = str(request.form['password'])
 
-    conn = sqlite3.connect("/app/highlands.db")
-
-    c = conn.cursor()
-    c.execute("SELECT username,password FROM users WHERE username='{un}' AND password='{pw}'".format(un=POST_USERNAME, pw=POST_PASSWORD))
-
-    rows = c.fetchall()
-
-    usernameCorrect = False
-    passwordCorrect = False
-    for row in rows:
-        for value in row:
-	    if POST_USERNAME == value:
-	        usernameCorrect = True
-	    elif POST_PASSWORD == value:
-	        passwordCorrect = True
-
-    result = False
-    if usernameCorrect == True and passwordCorrect == True:
-        result = True
+    result = User().login(POST_USERNAME, POST_PASSWORD)
 
     if result:
         session['logged_in'] = True
@@ -57,19 +89,7 @@ def logincheck():
         flash('Either your username or password was wrong. Try again.')
         return login()
 
-""" Database checks/setup """
-user_table_sql = 'CREATE TABLE `users`( `id` INTEGER PRIMARY KEY AUTOINCREMENT, `username` VARCHAR(75) NOT NULL, `password` VARCHAR(75) NOT NULL, `firstname` VARCHAR(75) NOT NULL, `lastname` VARCHAR(75) NOT NULL, `email` VARCHAR(75) NOT NULL)'
-default_user_sql = 'INSERT INTO users(username, password, firstname, lastname, email) VALUES("admin", "password", "Admin", "User", "admin@invalid.com")'
-if os.path.exists("/app/highlands.db") == False:
-    conn = sqlite3.connect("/app/highlands.db")
-    c = conn.cursor()
-    
-    c.execute(user_table_sql)
-    c.execute(default_user_sql)
-    
-    conn.commit()
-    conn.close()
-
 """ Start the Flask application """
 if __name__ == "__main__":
+    Database()
     app.run(debug=True, host='0.0.0.0', port=5000)
